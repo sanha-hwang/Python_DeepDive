f4c968647f0441ae935dead978a84e418eddd380
sanha-hwang <sanha805@gmail.com>
2022-01-30 11:42:46 +0900

    1주차 ver 1.1

--- 1주차/practice.py
+++ 1주차/practice.py
@@ -70,21 +70,33 @@ Quiz1) 변수를 이용하여 다음 문장을 출력하시오
 
 '''숫자형 deep'''
 
-print(1+1)
-print(3-2)
-print(5*2)
-print(6/3)
+# print(1+1)
+# print(3-2)
+# print(5*2)
+# print(6/3)
 
-print(2**3) #2^3=8
-print(5%3) # 나머지 구하기
-print(10%3)
-print(5//3) # 몫 구하기
-print(10//3)
+# print(2**3) #2^3=8
+# print(5%3) # 나머지 구하기
+# print(10%3)
+# print(5//3) # 몫 구하기
+# print(10//3)
 
-print(10 > 3)
-print(4 >= 7)
-print(10 <3 )
-print( 5<=5)
+# print(10 > 3)
+# print(4 >= 7)
+# print(10 <3 )
+# print( 5<=5)
 
-print( 3 == 3)
-print( 4 == 2)
+# print( 3 == 3)
+# print( 4 == 2)
+
+print(1 != 3)
+print(not(1 != 3))
+
+print((3 > 0) and (3 < 5)) # True
+print((3 > 0) & (3 < 5)) # True
+
+print((3 > 0) or (3 > 5)) # True
+print((3 > 0) | (3 > 5)) # True
+
+print(5 > 4 > 3) 
+print(5 > 4 > 7)
