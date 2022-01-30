570872c94ad339419f531df0411fc3b79005f0b1
sanha-hwang <sanha805@gmail.com>
2022-01-30 11:56:50 +0900

    1주차 ver1.2

--- 1주차/practice.py
+++ 1주차/practice.py
@@ -89,14 +89,90 @@ Quiz1) 변수를 이용하여 다음 문장을 출력하시오
 # print( 3 == 3)
 # print( 4 == 2)
 
-print(1 != 3)
-print(not(1 != 3))
+# print(1 != 3)
+# print(not(1 != 3))
 
-print((3 > 0) and (3 < 5)) # True
-print((3 > 0) & (3 < 5)) # True
+# print((3 > 0) and (3 < 5)) # True
+# print((3 > 0) & (3 < 5)) # True
 
-print((3 > 0) or (3 > 5)) # True
-print((3 > 0) | (3 > 5)) # True
+# print((3 > 0) or (3 > 5)) # True
+# print((3 > 0) | (3 > 5)) # True
 
-print(5 > 4 > 3) 
-print(5 > 4 > 7)
+# print(5 > 4 > 3) 
+# print(5 > 4 > 7)
+
+'''간단한 수식'''
+# print( 2 + 3*4) #14
+# print((2+3) * 4) # 20
+# number = 2 + 3*4
+# print(number)
+# number = number + 2
+# print(number)
+# number += 2
+# print(number)
+# number *= 2
+# print(number)
+# number /= 2
+# print(number)
+# number -= 2
+# print(number)
+
+# number %= 5
+# print(number)
+
+'''숫자 처리 함수'''
+# print(abs(-5)) #절대값 
+
+# print(pow(4,2)) # 4^2 = 16
+# print(max(5, 12)) #12
+# print(min(5,11)) #5
+# print(round(3.14)) #반올림
+# print(round(4.99))
+
+# from math import *
+
+# print(floor(4.99)) # 내림
+# print(ceil(3.14)) # 올림
+# print(sqrt(16)) # 제곱근
+
+'''랜덤함수'''
+
+# from random import *
+
+# print(random()) # 랜덤함수로 0.0~1.0미만의 난수 출력
+# print(random()*10)
+# print(int(random()*10))
+# print(int(random()*10)+1)
+
+# print(int(random() *45)+1)
+# print(int(random() *45)+1)
+# print(int(random() *45)+1)
+# print(int(random() *45)+1)
+# print(int(random() *45)+1)
+# print(int(random() *45)+1)
+
+# print(randrange(1,46)) #1~46 미만의 임의의 값 생성
+
+# print(randint(1,45)) # 1~45 이하의 임의의 값생성
+# print(randint(1,45)) # 1~45 이하의 임의의 값생성
+# print(randint(1,45)) # 1~45 이하의 임의의 값생성
+# print(randint(1,45)) # 1~45 이하의 임의의 값생성
+# print(randint(1,45)) # 1~45 이하의 임의의 값생성
+# print(randint(1,45)) # 1~45 이하의 임의의 값생성
+
+'''
+Quiz2) 당신은 최근에 코딩 스터디 모임을 새로 만들었습니다.
+월 4회 스터디를 하는데 3번은 온라인으로 하고 1번은 오프라인으로 하기로 했습니다.
+아래 조건에 맞은 오프라인 모임 날짜를 정해주는 프로그램을 작성하시오.
+
+조건1: 랜덤으로 날짜를 뽑아야함
+조건2: 월별 날짜는 다름을 감안하여 최소 일수인 28 이내로 정함
+조건3: 매월 1~3일은 스터디 준비를 해야 하므로 제외
+
+(출력문 예제)
+오프라인 스터디 모임 날짜는 매월 X일로 선정되었습니다.
+
+'''
+from random import *
+study = randint(4,28)
+print("오프라인 스터디 모임 날자는 매월", str(study) +"일로 선정되었습니다.")
