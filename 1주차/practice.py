8ea42acf843b35e41a21024cfb91ca4f0e83d911
sanha-hwang <sanha805@gmail.com>
2022-01-30 11:39:21 +0900

    1주차 ver1

---
+++ 1주차/practice.py
@@ -0,0 +1,90 @@
+'''숫자형'''
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+# print(5+3)
+# print(2*8)
+# print(3*(3+1))
+
+'''문자형'''
+# print('풍선')
+# print("나비")
+# print("ㅋ"*9)
+
+'''boolean 참/거짓'''
+# print(5>10)
+# print(5<10)
+# print(True)
+# print(False)
+# print(not True)
+# print(not (5 > 10))
+
+'''변수- 애완동물을 소개해 주세요'''
+
+# print("우리집 강아지의 이름은 연탄이에여")
+# print("연탄이는 4살이며, 산책을 아주 좋아해요")
+# print("연탄이는 어른일가요? True")
+
+# animal = "강아지"
+# name = "연탄이"
+# age = 4
+# hobby = "산책"
+# is_adult = (age >= 3)
+
+# print("우리집" + animal + "의 이름은" + name + "에여")
+# print(name + "는" +str(age) +"살이며," + hobby + "을 아주 좋아해요")
+# print(name + "는 어른일가요?" + str(is_adult))
+
+'''쉼표도 가능 대신 빈칸이 같이 들어감'''
+# animal = "고양이"
+# name = "까망이"
+# age = 2
+# hobby = "놀이"
+# is_adult = (age >= 3)
+
+# print("우리집",animal,"의 이름은",name,"에여")
+# print(name, "는",str(age) ,"살이며,",hobby, "을 아주 좋아해요")
+# print(name, "는 어른일가요?" ,str(is_adult))
+
+'''
+Quiz1) 변수를 이용하여 다음 문장을 출력하시오
+
+변수명:
+    station
+변수값:
+    "사당", "신도림", "인천공항" 순서대로 입력
+출력 문장:
+    XX 행 열차가 들어오고 있습니다.
+
+'''
+
+# station = "사당"
+# print(station+"행 열차가 들어오고 있습니다")
+
+# station = "신도림"
+# print(station+"행 열차가 들어오고 있습니다")
+
+# station = "인천공항"
+# print(station+"행 열차가 들어오고 있습니다")
+
+'''숫자형 deep'''
+
+print(1+1)
+print(3-2)
+print(5*2)
+print(6/3)
+
+print(2**3) #2^3=8
+print(5%3) # 나머지 구하기
+print(10%3)
+print(5//3) # 몫 구하기
+print(10//3)
+
+print(10 > 3)
+print(4 >= 7)
+print(10 <3 )
+print( 5<=5)
+
+print( 3 == 3)
+print( 4 == 2)
